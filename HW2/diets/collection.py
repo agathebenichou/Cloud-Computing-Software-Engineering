331f9c7967340741803f6008dcfef226e95d6fd1
AdamVinestock <115546705+AdamVinestock@users.noreply.github.com>
2023-06-14 13:35:56 +0300

    Converted Diets service to work on mongoDB

--- HW2/diets/collection.py
+++ HW2/diets/collection.py
@@ -1,40 +1,28 @@
-# import pymongo
-#
-# client = pymongo.MongoClient("mongodb://mongo:27017/")  # Establish a connection to MongoDB
-# db = client["nutrition"]                                # Access the "nutrition" database
-# dietscoll = db["diets"]                                 # Access the "diets" collection
-# result = dietscoll.insert_one({"_id":1, "value": "test - first value in diets DB"})
-#
-# # Print the inserted document
-# print(result.inserted_id)
-#
-# # Retrieve all documents in the collection
-# documents = dietscoll.find()
-# for document in documents:
-#     print(document)
-
+import pymongo
 
 class DietCollection:
-    """ DietCollection stores the old and performs operations on them
-    Each diet is stored in a dictionary with a unique numerical key called id,
-    and a value of the following: name, cal, sodium, sugar
+    """ DietCollection stores diets and performs operations on them
+    The diets DB is a collection held in the "nutrition" database.
+    Each diet document is stored with a unique numerical key called _id,
+    and values of the following: name, cal, sodium, sugar
     """
 
     def __init__(self):
-        self.opNum = 0
-        self.diets = {}
 
-        # client = pymongo.MongoClient("mongodb://mongo:27017/")   # Connect to the MongoDB server
-        # db = client["nutrition"]                                 # Access the database
-        #
-        # # Check if the "diets" collection exists, create it if it doesn't
-        # if "diets" not in db.list_collection_names():
-        #     db.create_collection("diets")
-        #
-        # self.diets = db["diets"]    # Access the "diets" collection
+        client = pymongo.MongoClient("mongodb://mongo:27017/")   # Connect to the MongoDB server
+        db = client["nutrition"]                                 # Access the database
 
-        # keynum = self.diets.find_one(sort=[("_id", -1)])["_id"]
-        # keynum +=1
+        # Check if the "diets" collection exists, create it if it doesn't
+        if "diets" not in db.list_collection_names():
+            db.create_collection("diets")
+
+        self.diets = db["diets"]    # Access the "diets" collection
+
+        latest_diet_id = self.diets.find_one(sort=[("_id", -1)])
+        if latest_diet_id is not None:
+            self.opNum = latest_diet_id["_id"]
+        else:
+            self.opNum = 0
 
     def retrieveAllDiets(self):
         """
@@ -42,9 +30,11 @@ class DietCollection:
         """
         print("DietCollection: retrieving all old:")
         diets_list = []
-        for diet in self.diets.values():
-            diet_without_id = diet.copy()  # Create a copy of the diet object
-            del diet_without_id["ID"]  # Remove the "ID" key from the copied object
+
+        cursor = self.diets.find()  # Retrieve all documents from the collection
+        for diet in cursor:
+            diet_without_id = diet.copy()
+            del diet_without_id["_id"]
             diets_list.append(diet_without_id)
         print(diets_list)
         return diets_list
@@ -52,42 +42,41 @@ class DietCollection:
     def insertDiet(self, diet_name, cal, sodium, sugar):
         """ Insert a new diet
         param: diet_name, cal, sodium, sugar
-        return: id of the new diet (key) and status code
+        return: id of the new diet (key)
         """
 
-        for id, diet in self.diets.items(): # check if diet exists, if so - return an error
-            if diet["name"] == diet_name:
-                print("Diet with name", diet_name, "already exists")
-                return 0
+        # Check if diet with the same name already exists in the collection
+        existing_diet = self.diets.find_one({"name": diet_name})
+        if existing_diet:
+            print("Diet with name", diet_name, "already exists")
+            return 0
 
         self.opNum += 1  # increment latest operation number
 
-        self.diets[self.opNum] = {
+        diet = {
             "name": diet_name,
-            "ID": self.opNum,
             "cal": cal,
             "sodium": sodium,
-            "sugar": sugar
+            "sugar": sugar,
+            "_id": self.opNum,
         }
-        print("Diet ", diet_name, " was created successfully")
 
+        # Insert the new diet document into the collection
+        result = self.diets.insert_one(diet)
+        if result.inserted_id:
+            print("Diet", diet_name, "was created successfully")
         return self.opNum
+
 
     def findDietName(self, name):
         """
         param name: the name of the diet
-        return: single JSON object of the diet specified by its name
+        return: Boolean if diet was found and BSON object of the diet specified by its name
         """
 
-        fetch_id = None
-        for id, diet in self.diets.items():
-            if diet["name"] == name:
-                fetch_id = id
-                break
+        diet = self.diets.find_one({"name": name})  # Find the diet by name in the collection
 
-        if fetch_id is not None: # Return diet from dictionary by key
-            print("DietCollection: found diet ", self.diets[fetch_id], " with id ", fetch_id)
-            return True, self.diets[fetch_id]
+        if diet is not None:
+            return True, diet
         else:
-            print(f"Diet {name} not found", name)
-            return False, None  # the key does not exist in the collection
+            return False, None
