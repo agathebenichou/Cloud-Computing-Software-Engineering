c564bf3c2e2156b832c732fb4f74100b2205ea79
Agathe Benichou <agatha.benichou@gmail.com>
2023-06-19 13:24:09 +0300

    addig hw3

---
+++ HW3/collection.py
@@ -0,0 +1,337 @@
+import requests
+
+class DishCollection:
+    """ DishCollection stores the dishes and performs operations on them
+        Each dish is stored in a dictionary with a unique numerical key called id,
+        and a value of the following:
+            dish name, calories, size (default 100g), sodium, suger
+    """
+
+    def __init__(self):
+
+        self.opNum = 0
+        self.dishes = {}
+
+    def retrieveAllDishes(self):
+        """
+        Retrieve all dicts containing dishes insertDish
+        :return: dictionary of all dishes in the collection
+        """
+        print("DishCollection: retrieving all dishes:")
+        print(self.dishes)
+
+        return self.dishes
+
+    def insertDish(self, dish_name):
+        """
+        Insert a new dish based on dish name
+        param: dish_name
+        return: id of the new dish (key) and status code
+        """
+
+        # Iterate over existing dishes collection and check if dish with same name already exists
+        print("Checking if dish already exists")
+        for id, dish in self.dishes.items():
+            if dish["name"] == dish_name: # If dish already exists, returns an error
+                print("DishCollection: dish ", dish_name, " already exists")
+                return -2
+
+        try:
+            # Query API Ninja /nutrition
+            api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(dish_name)
+            response = requests.get(api_url, headers={'X-Api-Key': '6zoIr+IoEg7H2GQGVDxw+g==WdtcKEIt1DOIoGKj'})
+
+            if response.status_code == requests.codes.ok: # Check status code of response
+                dish_data = response.json()
+
+                # If dish not recognized by api/ninja
+                if not dish_data:
+                    print("Api Ninja/Nutrition does not recognize dish name:", dish_data)
+                    return -3
+
+                else:
+
+                    # Iterate over all dishes to accumulate components
+                    total_calories, total_sodium, total_sugar, total_serving_size = 0, 0, 0, 0
+                    for _dish in dish_data:
+                        total_calories += _dish["calories"]
+                        total_sodium += _dish["sodium_mg"]
+                        total_sugar += _dish["sugar_g"]
+                        total_serving_size += _dish["serving_size_g"]
+
+                    # Add dish to dish collection
+                    self.dishes[self.opNum] = {
+                        "name": dish_name,
+                        "ID": self.opNum,
+                        "cal": total_calories,
+                        "size": total_serving_size,
+                        "sodium": total_sodium,
+                        "sugar": total_sugar
+                    }
+                    print(self.dishes)
+                    print("DishCollection: dish ", dish_name, " was added")
+
+                    self.opNum += 1  # increment latest operation number
+
+            else:
+                print(f"Api Ninja/Nutrition not reachable: {response.status_code}, {response.text}")
+                return -4
+        except Exception as e:
+            print(f"Api Ninja/Nutrition not reachable: {e}")
+            return -4
+
+        return self.opNum
+
+    def findDishID(self, id):
+        """
+        Return a single JSON object of the dish specified by its ID
+        :param id: the ID of the dish
+        :return: JSON object of the dish
+        """
+
+        if id in self.dishes.keys():  # the id exists in collection
+            d = self.dishes[id]
+            print("DishCollection: found dish ", d, " with id ", id)
+            return True, d
+        else: # the id does not exist in the collection
+            print("DishCollection: did not find id", id)
+            return False, None
+
+    def delDishID(self, id):
+
+        if id in self.dishes.keys():  # the key exists in collection
+            d = self.dishes[id]
+            del self.dishes[id]
+            print("DishCollection: deleted dish ", d, " with id ", id)
+            return True, id
+
+        else: # the key does not exist in the collection
+            return False, None
+
+    def delDishName(self, name):
+
+        id_to_delete = None
+        for id, dish in self.dishes.items():
+            if dish["name"] == name:
+                id_to_delete = id
+                break
+
+        # Delete dish from dictionary by key
+        if id_to_delete is not None:
+            del self.dishes[id_to_delete]
+            print("DishCollection: deleted dish with name ", name)
+            return True, id_to_delete
+        else:
+            print("DishCollection: did not find dish_name ", name)
+            return False, None
+
+    def findDishName(self, name):
+        """
+        Return a single JSON object of the dish specified by its name
+        param name: the name of the dish
+        return: value of the dish
+        """
+
+        fetch_id = None
+        for id, dish in self.dishes.items():
+            if dish["name"] == name:
+                fetch_id = id
+                break
+
+        if fetch_id is not None: # Return dish from dictionary by key
+            print("DishCollection: found dish ", self.dishes[fetch_id], " with id ", fetch_id)
+            return True, self.dishes[fetch_id]
+        else:
+            print("DishCollection: did not find dish_name ", name)
+            return False, None  # the key does not exist in the collection
+
+    def checkDishes(self, list_of_ids):
+        """
+        Checks if all IDs in a list exist in dishes
+        :params: list of dish IDs
+        :return: True or False depending on results
+        """
+
+        exists = all(elem in self.dishes.keys() for elem in list_of_ids)
+        print(f"All dishes exist: {exists}")
+        return exists
+
+class MealCollection:
+    """ MealCollection stores the dishes and performs operations on them
+        Each meal is stored in a dictionary with a unique numerical key called id,
+        and a value of the following:
+            name, ID, appetizer, main, dessert, cal, sodium, sugar
+    """
+
+    def __init__(self):
+        # self.opNum is the number of insertMeal operations performed
+        self.opNum = 0
+
+        # self.old is a dictionary of the form {key:meal} where key is an integer and meal is a list JSON objects
+        self.meals = {}
+
+
+    def retrieveAllMeals(self):
+        """
+        Retrieve all dicts containing old
+        :return: dictionary of all old in the collection
+        """
+        print("MealCollection: retrieving all old:")
+        print(self.meals)
+
+        return self.meals
+
+    def updateMeals(self, dish_id):
+        """ Given a dish_id, update the old
+
+        :param dish_id: dish ID being deleted
+        """
+
+        for id, meal in self.meals.items(): # iterate over all dishes in a meal
+            delete_components = False
+
+            # null out the dish ID that was deleted and associated with the meal
+            if dish_id == meal["appetizer"]:
+                meal["appetizer"] = None
+                delete_components = True
+            elif dish_id == meal["main"]:
+                meal["main"] = None
+                delete_components = True
+            elif dish_id["dessert"]:
+                dish_id["dessert"] = None
+                delete_components = True
+
+            if delete_components: # if a dish ID was deleted, null out the components
+                meal["cal"], meal["sodium"], meal["sugar"] = None, None, None
+
+    def insertMeal(self, meal_name, appetizer_id, main_id, dessert_id, disheColl):
+        """"
+        Insert a meal given the name and the corresponding dish IDs. To create a meal, it
+        computes the total numebr of calories, sodium, sugar.
+
+        :param: meal name and component dish IDs
+        :returns: the ID of the created meal
+        """
+
+        for id, meal in self.meals.items(): # check if meal exists, if so - return an error
+            if meal["name"] == meal_name:
+                print("MealCollection: meal ", meal_name, " already exists")
+                return -2
+
+        self.opNum += 1  # increment latest operation number
+
+        self.meals[self.opNum] = { # sum over all components
+            "name": meal_name,
+            "ID": self.opNum,
+            "appetizer": appetizer_id,
+            "main": main_id,
+            "dessert": dessert_id,
+            "cal": sum(cal for cal in [disheColl.dishes[appetizer_id]["cal"], disheColl.dishes[main_id]["cal"], disheColl.dishes[dessert_id]["cal"]]),
+            "sodium": sum(sodium for sodium in [disheColl.dishes[appetizer_id]["sodium"], disheColl.dishes[main_id]["sodium"], disheColl.dishes[dessert_id]["sodium"]]),
+            "sugar": sum(sugar for sugar in [disheColl.dishes[appetizer_id]["sugar"], disheColl.dishes[main_id]["sugar"], disheColl.dishes[dessert_id]["sugar"]])
+        }
+        print("MealCollection: meal ", meal_name, " was added")
+
+        return self.opNum
+
+    def delMealID(self, id):
+        """" Given a meal ID, delete it from the colleciton
+
+        :params: the ID of a meal to delete
+        :returns: True if successfully deleted, False if not found
+        """
+
+        if id in self.meals.keys():  # the key exists in collection
+            d = self.meals[id]
+            del self.meals[id]
+            print("MealCollection: deleted meal ", d, " with id ", id)
+            return True, d
+        else:
+            return False, None  # the key does not exist in the collection
+
+    def findMealID(self, id):
+        """ Given a meal ID, find the resulting collection
+
+        :params: the ID of the meal to find
+        :returns: True if found, False if not
+        """
+        if id in self.meals.keys():  # the key exists in collection
+            d = self.meals[id]
+            print("MealCollection: found meal ", d, " with id ", id)
+            return True, d
+        else:
+            print("MealCollection: did not find id", id)
+            return False, None  # the key does not exist in the collection
+
+    def delMealName(self, name):
+        """ Given a meal name, delete the meal
+
+        :params: the name of the meal to delete
+        :returns: True if successfully deleted (and its ID), False if not
+        """
+
+        id_to_delete = None
+        for id, meal in self.meals.items(): #search for the meal ID given the name
+            if meal["name"] == name:
+                id_to_delete = id
+                break
+
+        # Delete dish from dictionary by key
+        if id_to_delete is not None:
+            del self.meals[id_to_delete]
+            print("MealCollection: deleted meal with name ", name)
+            return True, id_to_delete
+        else:
+            print("MealCollection: did not find meal_name ", name)
+            return False, None
+
+    def findMealName(self, name):
+        """
+         Returns a single JSON object of the meal specified by its name
+         param name: the name of the meal
+         return: value of the meal
+         """
+
+        fetch_id = None
+        for id, meal in self.meals.items(): #search for the meal ID given the name
+            if meal["name"] == name:
+                fetch_id = id
+                break
+
+        if fetch_id is not None:  # Return meal from dictionary by key
+            print("MealCollection: found meal ", self.meals[fetch_id], " with id ", fetch_id)
+            return True, self.meals[fetch_id]
+        else:
+            print("MealCollection: did not find meal_name ", name)
+            return False, None  # the key does not exist in the collection
+
+    def replaceMeal(self, id, meal_name, appetizer_id, main_id, dessert_id, disheColl):
+        """ Given a meal ID, replaces the meal components with the new meal name and component IDs
+
+        :params: ID of meal to replace and new components (name and IDs)
+        :returns: True if updated, False if meal was not in collection
+        """
+
+        if id in self.meals.keys():  # the key exists in collection
+            self.meals[id] = {
+                "name": meal_name,
+                "ID": id,
+                "appetizer": appetizer_id,
+                "main": main_id,
+                "dessert": dessert_id,
+                "cal": sum(cal for cal in [disheColl.dishes[appetizer_id]["cal"], disheColl.dishes[main_id]["cal"],
+                                           disheColl.dishes[dessert_id]["cal"]]),
+                "sodium": sum(sodium for sodium in
+                              [disheColl.dishes[appetizer_id]["sodium"], disheColl.dishes[main_id]["sodium"],
+                               disheColl.dishes[dessert_id]["sodium"]]),
+                "sugar": sum(sugar for sugar in
+                             [disheColl.dishes[appetizer_id]["sugar"], disheColl.dishes[main_id]["sugar"],
+                              disheColl.dishes[dessert_id]["sugar"]])
+            }
+            print("MealCollection: New meal for id ", id, " is ", self.meals[id])
+
+            return True, self.meals[id]
+
+        else:  # the key does not exist in the collection
+            print("MealCollection: did not find id", id)
+            return False, None
---
+++ HW3/model.py
@@ -0,0 +1,358 @@
+from flask_restful import Resource
+from collection import DishCollection, MealCollection
+from flask import request
+
+"""
+The resources are:
+
+- /meals                            This is a collection class, containing all the old
+- /mealsID} or /meals/{name}      Each meal resource is expressed with a specific JSON object
+- /dishes                           This is a collection class, containing all the dishes 
+- /dishes/{ID} or /dishes/{name}    Each dish resource is expressed with a specific JSON object
+"""
+
+
+dishColl = DishCollection()
+
+
+class Dishes(Resource):
+    """
+    The Dishes class implements the REST operations for the /dishes resource
+    /dishes
+        POST (add a dish of the given name)
+        GET (return the JSON object listing all dishes, indexed by ID)
+    """
+
+    global dishColl
+
+    def get(self):
+        """
+        Retrieves a specific dish from the collection
+        :param key: the key used to search for a dish (either ID or name)
+        :return: JSON object listing all dishes (indexed by ID) and the status code
+        """
+
+        return dishColl.retrieveAllDishes(), 200
+
+    def post(self):
+        """
+        Adds a dish to /dishes and returns its ID
+        :param key: name used to add a dish
+        :return: id: dish ID given to the dish
+        """
+
+        # if request content-type is not application/json
+        if 'Content-Type' not in dict(request.headers).keys():
+            print("Request Content-Type not specified in header")
+            return 0, 415
+        else:
+            if dict(request.headers)['Content-Type'] != "application/json":
+                print("Request Content-Type is not application/json")
+                return 0, 415
+
+        data = request.json # accept data as json
+
+        # if body is not of type dict
+        if type(data) != dict:
+            return 0, 415
+        else:
+            # if name is empty or spelled wrong
+            if 'name' not in data.keys():
+                return -1, 422
+            else:
+                d = data['name']
+
+        id = dishColl.insertDish(d)  # add d to collection
+
+        if id == -2:   # dish already exists
+            return id, 422
+        elif id == -3:  # api.api-ninjas.com/v1/nutrition does not recognize dish name
+            return id, 422
+        elif id == -4:  # api.api-ninjas.com/v1/nutrition was not reachable
+            return -4, 504
+        return id, 201
+
+    def delete(self):
+        """ Deleting the entire collection is not allowed
+
+        :return: return error message
+        """
+
+        return "This method is not allowed for request URL", 405
+
+
+class DishesID(Resource):
+    """ Implements the REST operations for the /dishes/{ID} resource
+
+    /dishes/{ID}
+        GET (return the JSON object of the dish given the ID)
+        DELETE (delete a dish of the given ID)
+    """
+
+    global dishColl
+    global mealColl
+
+    def get(self, id):
+        """ Retrieve a specific dish from the collection based off its ID
+
+        :param id: the ID of the dish to retrieve
+        :return: the dish JSON object and the status code
+        """
+
+        (status, dish_obj) = dishColl.findDishID(id)
+        if status: # return the word and HTTP 200 ok code
+            return dish_obj, 200
+        else: # if dish not found
+            return -5, 404
+
+    def delete(self, id):
+        """ Delete a dish from the collection based off its ID
+
+        :param id: the ID of the dish to delete
+        :return: the deleted dish ID and the status code
+        """
+
+        (status, dish_id) = dishColl.delDishID(id)
+        if status: # return deleted dish and HTTP 200 ok code
+
+            # update all old to delete dish ID
+            mealColl.updateMeals(dish_id)
+
+            return dish_id, 200
+        else: # return 0 for id value (error) and Not Found error code
+            return -5, 404
+
+
+class DishesName(Resource):
+    """ Implements the REST operations for the /dishes/{name} resource
+
+    /dishes/{name}
+        GET (return the JSON object of the dish given the name)
+        DELETE (delete a dish of the given name)
+    """
+
+    global dishColl
+    global mealColl
+
+    def delete(self, name):
+        """ Delete a dish from the collection based off its name
+
+        :param name: the nsmr of the dish to delete
+        :return: the deleted dish and the status code
+        """
+
+        (status, dish_id) = dishColl.delDishName(name)
+        if status: # return deleted word and HTTP 200 ok code
+
+            # Update meal based off of
+            mealColl.updateMeals(dish_id)
+
+            return dish_id, 200
+        else: # return 0 for key value (error) and Not Found error code
+            return -5, 404
+
+    def get(self, name):
+        """ Retrieve a specific dish from the collection based off its ID
+
+        :param name: the name of the dish to retrieve
+        :return: the dish and the status code
+        """
+
+        (status, dish_obj) = dishColl.findDishName(name)
+        if status: # return the word and HTTP 200 ok code
+            return dish_obj, 200
+        else: # return 0 for key and Not Found error code
+            return -5, 404
+
+# create MealCollection instance with global scope
+mealColl = MealCollection()
+
+class Meals(Resource):
+    """ The Meal class implements the REST operations for the /old resource
+
+    /old
+        POST (add a meal of the given name)
+        GET (return the JSON object listing all old, indexed by ID)
+    """
+
+    global dishColl
+    global mealColl
+
+    def get(self):
+        """ Retrieves a specific meal from the collection
+
+        :return: all meal objects and status code
+        """
+
+        return mealColl.retrieveAllMeals(), 200
+
+    def post(self):
+        """
+        Adds a meal to /old given a JSON object with fields: name, appetizer, main, dessert
+        :return: id: ID given to the created meal
+        """
+
+        # if request content-type is not application/json
+        if 'Content-Type' not in dict(request.headers).keys():
+            print("Request Content-Type not specified in header")
+            return 0, 415
+        else:
+            if dict(request.headers)['Content-Type'] != "application/json":
+                print("Request Content-Type is not application/json")
+                return 0, 415
+
+        data = request.json
+
+        # if body is not of type dict
+        if type(data) != dict:
+            return 0, 415
+        else:
+
+            # not all keys are present
+            keys = ['name', 'appetizer', 'main', 'dessert']
+            all_present = all(elem in keys for elem in data.keys())
+
+            if not all_present:
+                print(f"One of the required parameters was not specified")
+                return -1, 422
+            else:
+                meal_name = data['name']
+                appetizer_id = data['appetizer']
+                main_id = data['main']
+                dessert_id = data['dessert']
+
+        dishes_exists = dishColl.checkDishes([appetizer_id, main_id, dessert_id])
+        if dishes_exists:
+
+            # add meal to collection (after check for dish existence)
+            key = mealColl.insertMeal(meal_name, appetizer_id, main_id, dessert_id, dishColl)
+            if key == -2:  # meal already exists
+                return -2, 422
+            return key, 201
+
+        else:  # one of the dish IDs does not exist
+            return -6, 422
+
+class MealsID(Resource):
+    """ Implements the REST operations for the /old/{ID} resource
+
+    /old/{ID}
+        GET (return the JSON object of the meal given the ID)
+        DELETE (delete a meal of the given ID)
+        PUT (add a meal of the given ID)
+    """
+
+    global mealColl
+    global dishColl
+
+    def delete(self, id):
+        """ Delete a meal from the collection based off its ID
+
+        :param id: the ID of the meal to delete
+        :return: the deleted meal and the status code
+        """
+        b, w = mealColl.delMealID(id)
+        if b:
+            return w, 200  # return deleted meal and HTTP 200 ok code
+        else:
+            return 5, 404  # return 0 for key value (error) and Not Found error code
+
+    def get(self, id):
+        """
+        Retrieve a specific meal from the collection based off its ID
+        :param id: the ID of the meal to retrieve
+        :return: the meal and the status code
+        """
+
+        (b, m) = mealColl.findMealID(id)
+        if b:  # return the meal and HTTP 200 ok code
+            return m, 200
+        else:  # return -5 for key and Not Found error code
+            return -5, 404
+
+    def put(self, id):
+        """ Modifies a meal associated with a specific ID
+
+        :param id: the ID of the meal to be modified
+        :return: the JSON object of the modified meal
+        """
+
+        # if request content-type is not application/json
+        if 'Content-Type' not in dict(request.headers).keys():
+            print("Request Content-Type not specified in header")
+            return 0, 415
+        else:
+            if dict(request.headers)['Content-Type'] != "application/json":
+                print("Request Content-Type is not application/json")
+                return 0, 415
+
+        data = request.json
+
+        # if body is not of type dict
+        if type(data) != dict:
+            return 0, 415
+        else:
+
+            # not all keys are present
+            keys = ['name', 'appetizer', 'main', 'dessert']
+            all_present = all(elem in keys for elem in data.keys())
+
+            if not all_present:
+                print(f"One of the required parameters was not specified")
+                return -1, 422
+            else:
+                meal_name = data['name']
+                appetizer_id = data['appetizer']
+                main_id = data['main']
+                dessert_id = data['dessert']
+
+        dishes_exists = dishColl.checkDishes([appetizer_id, main_id, dessert_id])
+        if dishes_exists:
+
+            # replace the word in the collection
+            b, w = mealColl.replaceMeal(id, meal_name, appetizer_id, main_id, dessert_id, dishColl)
+            if b: # return the word and HTTP 200 ok code
+                return w, 200
+
+            else: #return 0 for key and Not Found error code
+                return -5, 404
+
+        else:  # one of the dish IDs does not exist
+            return -6, 422
+
+
+class MealsName(Resource):
+    """ Implements the REST operations for the /old/{name} resource
+
+    /old/{name}
+        GET (return the JSON object of the meal given the name)
+        DELETE (delete a meal of the given name)
+    """
+
+    global mealColl
+
+    def delete(self, name):
+        """ Delete a meal from the collection based off its name
+
+        :param name: the meal of the meal to delete
+        :return: the deleted meal and the status code
+        """
+
+        b, w = mealColl.delMealName(name)
+        if b:
+            return w, 200  # return deleted word and HTTP 200 ok code
+        else:
+            return -5, 404  # return 0 for key value (error) and Not Found error code
+
+    def get(self, name):
+        """ Retrieve a specific meal from the collection based off its ID
+
+        :param id: the ID of the meal to retrieve
+        :return: the meal and the status code
+        """
+
+        (b, w) = mealColl.findMealName(name)
+        if b:
+            return w, 200  # return the word and HTTP 200 ok code
+        else:
+            return -5, 404  # return 0 for key and Not Found error code
