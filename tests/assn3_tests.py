e075a59f9fb27afcee4834ad416d30dab145d8e8
AdamVinestock <115546705+AdamVinestock@users.noreply.github.com>
2023-07-16 19:32:58 +0300

    test 1-4 work
    I wrote test1-4 (there are 8 in total).
    I didnt manage to import the files from hw1 tests to I duplicated, this is ugly we should fix it.

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -1,8 +1,4 @@
-# from original_hw1_tests import connectionController, assertions
-
-from original_hw1_tests.assertions import *
-from original_hw1_tests.restApiController import *
-
+from restApiController import *
 
 # tests if POST /dishes is valid (201 status_code and all have different IDs)
 def test_1():
@@ -10,8 +6,8 @@ def test_1():
 
     # Post 3 dishes and collect their IDs
     for dish in ["orange", "spaghetti", "apple pie"]:
-        response = add_dish(dish) # in add_dish we check for 201 status_code, the response is of type json
-        dish_id = response["ID"]
+        response = add_dish(dish) # in add_dish we check for 201 status_code, the response is the ID of the new dish
+        dish_id = response
         dish_ids.append(dish_id)
 
     # Check if all dish ID's are different
@@ -20,7 +16,24 @@ def test_1():
 
 # tests if GET /dishes/<orange-ID> sodium is 0.9-1.1 and status_code is 200
 def test_2():
-    orange_id = add_dish("orange")
+    orange_id = 1 # orange ID is 1 since it's the first dish we add
     response = connectionController.http_get(f"dishes/{orange_id}")
     assert_err_code(response, error_code=200)
     assert 0.9 <= response.json()["sodium"] <= 1.1
+
+# tests if GET /dishes return all 3 dishes and status_code 200
+def test_3():
+    response = connectionController.http_get("dishes")
+    dishes = response.json()
+    assert_err_code(response, error_code=200)
+    assert len(dishes) == 3
+
+# tests if POST /dishes/"blah" return -3 and status code 404,400 or 422
+def test_4():
+    dish = {"name": "blah"}
+    response = connectionController.http_post("dishes",  dish)
+    assert response.status_code in [404, 400, 422]
+    assert_ret_value(response, returned_value=-3)
+
+
+
